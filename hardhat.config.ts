16f750b9cbc25cc8d3f5cb1b4dabf6327919ee93
beauwilliams <beau.williams@icloud.com>
2022-07-17 23:11:13 +1000

    fix: update compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,7 +40,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.2",
+        version: "0.8.13",
         settings: {
           optimizer: {
             enabled: true,
